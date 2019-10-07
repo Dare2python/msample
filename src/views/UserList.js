32ae577bfc1b65846150773b3ca3107d97f57c41
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-07 16:26:07 +0300

    routing and UserForm

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -5,7 +5,10 @@ module.exports = {
     oninit: User.loadList,
     view: function() {
         return m(".user-list", User.list.map(function(user) {
-            return m(".user-list-item", user.firstName + " " + user.lastName)
+            return m(m.route.Link, {
+                class: "user-list-item",
+                href: "/edit/" + user.id,
+            }, user.firstName + " " + user.lastName)
         }))
     }
 }
